88b08ece3a2d528a43c1bf22814fae04aad1aef1
Sascha Dittmann <Sascha5812@gmx.de>
2016-09-11 15:04:17 +0200

    Java and Zookeeper Versions updated

--- kafka-on-redhat/install-zookeeper-cluster.sh
+++ kafka-on-redhat/install-zookeeper-cluster.sh
@@ -118,12 +118,12 @@ log "Installing Java"
 
 # redhat java install
 cd /temp
-wget --no-cookies --no-check-certificate --header "Cookie: gpw_e24=http%3A%2F%2Fwww.oracle.com%2F; oraclelicense=accept-securebackup-cookie" "http://download.oracle.com/otn-pub/java/jdk/8u45-b14/jdk-8u45-linux-x64.rpm"
-wget --no-cookies --no-check-certificate --header "Cookie: gpw_e24=http%3A%2F%2Fwww.oracle.com%2F; oraclelicense=accept-securebackup-cookie" "http://download.oracle.com/otn-pub/java/jdk/8u45-b14/jre-8u45-linux-x64.rpm"
+wget --no-cookies --no-check-certificate --header "Cookie: gpw_e24=http%3A%2F%2Fwww.oracle.com%2F; oraclelicense=accept-securebackup-cookie" "http://download.oracle.com/otn-pub/java/jdk/8u102-b14/jdk-8u102-linux-x64.rpm"
+wget --no-cookies --no-check-certificate --header "Cookie: gpw_e24=http%3A%2F%2Fwww.oracle.com%2F; oraclelicense=accept-securebackup-cookie" "http://download.oracle.com/otn-pub/java/jdk/8u102-b14/jre-8u102-linux-x64.rpm"
 
 #Install packages
-rpm -Uvh jdk-8u45-linux-x64.rpm
-rpm -Uvh jre-8u45-linux-x64.rpm
+rpm -Uvh jdk-8u102-linux-x64.rpm
+rpm -Uvh jre-8u102-linux-x64.rpm
 
 # Install Zookeeper
 log "Installing Zookeeper"
